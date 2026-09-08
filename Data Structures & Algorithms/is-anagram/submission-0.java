2071e04e5b814ebb8d83a8ea7246c443bbdc59b3
bikashkumar00004-rgb <bikashkumar00004@gmail.com>
2026-09-08 08:14:29 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m =s.length();
+        int n =t.length();
+        if(m != n) return false;
+
+        
+        HashMap<Character,Integer> map1 =new HashMap<>();
+        HashMap<Character,Integer> map2 =new HashMap<>();
+
+        for(int i =0;i<n;i++){
+                map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i),0)+1);
+            
+                map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i),0)+1);
+            
+        }
+        return map1.equals(map2);
+
+    }
+}
